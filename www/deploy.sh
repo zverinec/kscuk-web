4f228c1ee402aa8eb2a31f52c861b836a57c1e2c
Jan Horacek <me@apophis.cz>
2018-03-20 12:18:09 +0100

    Add deploy script.

---
+++ www/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "Content-type: text/html\n"
+
+OUTPUT+=$(git fetch origin 2>&1)$'\n\n'
+if [ $? -eq 0 ]; then
+	OUTPUT+=$(git reset --hard origin/master 2>&1)$'\n\n'
+fi
+
+if [ $? -eq 0 ]; then
+	OUTPUT+=$(cd .. && rm -rf ./temp/cache 2>&1)$'\n\n'
+fi
+
+echo "$OUTPUT" | mail "me@apophis.cz" -s "[kscuk-web] Deploy status"
