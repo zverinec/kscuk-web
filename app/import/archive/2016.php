1aad35b489e97da1bff9ef9d2884bce244d66f62
Vendula Němcová <v.nem.co@gmail.com>
2018-08-04 19:47:55 +0200

    Add array version of data.

---
+++ app/import/archive/2016.php
@@ -0,0 +1,39 @@
+<?php
+$year = [
+	"year" => 2016,
+	"when" => "11.–18. září 2016",
+	"where" => [
+		"place" => "Mohelenský mlýn",
+		"map" => "https://mapy.cz/s/28VF9"
+	],
+	"who" => [
+		["person" => "Honza Horáček", "team" => "ksi"],
+		["person" => "Honza Mrázek", "team" => "ksi"],
+		["person" => "Lukáš Chrást", "team" => "ibis"],
+		["person" => "Pavla Havlíčková", "team" => "ibis"],
+		["person" => "Dominika Krejčí", "team" => "ksi"],
+		["person" => "Tom Effenberger", "team" => "ksi"],
+		["person" => "Henrich Heňo Lauko", "team" => "ksi"],
+		["person" => "Martin Tíman Hanžl", "team" => "ksi"],
+		["person" => "Jiří Novotný", "team" => "ksi"]
+	],
+	"story" => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris suscipit, ligula sit amet pharetra "
+		."semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Duis condimentum augue id magna "
+		."semper rutrum. Nunc dapibus tortor vel mi dapibus sollicitudin. In laoreet, magna id viverra "
+		."tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Maecenas sollicitudin.",
+	"gallery" => [
+		"IMG_7810.JPG",
+		"IMG_0162.JPG",
+		"IMG_0172.JPG",
+		"IMG_0259.JPG",
+		"IMG_0341.JPG",
+		"IMG_0618.JPG",
+		"IMG_0787.JPG",
+		"IMG_0861.JPG",
+		"IMG_0864.JPG",
+		"IMG_1099.JPG",
+		"IMG_1215.JPG",
+		"IMG_6540.jpg"
+	],
+	"more-story" => ""
+];
---
+++ app/import/archive/2017.php
@@ -0,0 +1,39 @@
+<?php
+$year = [
+	"year" => 2017,
+	"when" => "3.–10. září 2017",
+	"where" => [
+		"place" => "Strážná u Lelekovic",
+		"map" => "https://mapy.cz/s/2UPis"
+	],
+	"who" => [
+		["person" => "Martina Krasnayová", "team" => "ksi"],
+		["person" => "Honza Horáček", "team" => "ksi"],
+		["person" => "Honza Mrázek", "team" => "ksi"],
+		["person" => "Lukáš Chrást", "team" => "ibis"],
+		["person" => "Pavla Havlíčková", "team" => "ibis"],
+		["person" => "Dominika Krejčí", "team" => "ksi"],
+		["person" => "Jan Pokorný", "team" => "ksi"],
+		["person" => "David Dobrovolný", "team" => "ksi"],
+		["person" => "Béďa Said", "team" => "ksi"]
+	],
+	"story" => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris suscipit, ligula sit amet pharetra "
+		."semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Duis condimentum augue id magna "
+		."semper rutrum. Nunc dapibus tortor vel mi dapibus sollicitudin. In laoreet, magna id viverra "
+		."tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Maecenas sollicitudin.",
+	"gallery" => [
+		"IMG_7223.jpg",
+		"IMG_7304.jpg",
+		"IMG_7418.jpg",
+		"IMG_3856.JPG",
+		"IMG_3870.JPG",
+		"IMG_3956.JPG",
+		"IMG_4093.JPG",
+		"IMG_4155.jpg",
+		"IMG_4179.jpg",
+		"IMG_4216.jpg",
+		"IMG_4267.jpg",
+		"IMG_7167.jpg"
+	],
+	"more-story" => ""
+];
