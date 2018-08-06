1c787478a84d5fe3beb79192fcb70ea03a6b776a
Jan Horacek <me@apophis.cz>
2018-08-06 13:26:14 +0200

    Archive: add year 2015.

---
+++ app/data/archive/2015.php
@@ -0,0 +1,40 @@
+<?php
+$year = [
+	"year" => 2015,
+	"when" => "6.–13. září 2015",
+	"where" => [
+		"place" => "Mohelenský mlýn",
+		"map" => "https://mapy.cz/s/28VF9"
+	],
+	"who" => [
+		["person" => "Honza Drábek", "team" => "ksi"],
+		["person" => "Jiří Maara Marek", "team" => "ksi"],
+		["person" => "Marta", "team" => "ibis"],
+		["person" => "Martina Tinka Krasnayová", "team" => "ksi"],
+		["person" => "Martin Tíman Hanžl", "team" => "ksi"],
+		["person" => "Honza Mrázek", "team" => "ksi"],
+		["person" => "Henrich Heňo Lauko", "team" => "ksi"],
+		["person" => "Tom Effenberger", "team" => "ksi"],
+		["person" => "Lukáš Chrást", "team" => "ibis"],
+		["person" => "Anička", "team" => "ibis"],
+	],
+	"story" => "Mohleno, Jihlava, hra o čas, RSA key signing párty, projekty, "
+		."Los Karlos, galavečer, eurofólie, poprvé v nové budově mlýna, "
+		."megahrátky, informatické i biologické projekty, hra epidemie, "
+		." přespání v přírodě, ctvrčci. KSI a KEKS.",
+	"gallery" => [
+		"DSC_0227.JPG",
+		"DSC_0301.JPG",
+		"DSC_0405.JPG",
+		"DSC_0416.JPG",
+		"DSC_0420.JPG",
+		"DSC_0590.JPG",
+		"DSC_0654.JPG",
+		"DSC_0672.JPG",
+		"DSC_0747.JPG",
+		"DSC_1025.JPG",
+		"DSC_1294.JPG",
+		"DSC_1314.JPG",
+	],
+	"more-story" => ""
+];
