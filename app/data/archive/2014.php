bca6218556f0d8317c913193a7144e0131bf443a
Jan Horacek <me@apophis.cz>
2018-08-10 12:00:12 +0200

    Archove: add 2014 year.

---
+++ app/data/archive/2014.php
@@ -0,0 +1,39 @@
+<?php
+$year = [
+	"year" => 2014,
+	"when" => "6.–13. září 2014",
+	"where" => [
+		"place" => "Fara Bedřichov",
+		"map" => "https://en.mapy.cz/s/2Vmt5"
+	],
+	"who" => [
+		["person" => "Jiří Maara Marek", "team" => "ksi"],
+		["person" => "Tom Effenberger", "team" => "ksi"],
+		["person" => "Evka", "team" => "ibis"],
+		["person" => "Lukáš Chrást", "team" => "ibis"],
+		["person" => "Tom Jadrný", "team" => "ksi"],
+		["person" => "Honza Mrázek", "team" => "ksi"],
+		["person" => "Henrich Heňo Lauko", "team" => "ksi"],
+		["person" => "Honza Drábek", "team" => "ksi"],
+		["person" => "Martina Tinka Krasnayová", "team" => "ksi"],
+		["person" => "Martin Tíman Hanžl", "team" => "ksi"],
+	],
+	"story" => "Rybník, UFO, deadlock, fara, koule a štíty, Generál Nolič, "
+		."legendární přednáška „Jak sbalit informatičku”, old school hry, "
+		."spousta světýlek, Ovčilka, Béďa v kuchyni a trojjaká rýže.",
+	"gallery" => [
+		"04200003.001.0.jpg",
+		"04200003.008.0.jpg",
+		"04200003.009.0.jpg",
+		"04200003.010.0.jpg",
+		"04200003.012.0.jpg",
+		"04200003.013.0.jpg",
+		"04200003.014.0.jpg",
+		"04200003.016.0.jpg",
+		"04200003.018.0.jpg",
+		"04200003.021.0.jpg",
+		"04200003.022.0.jpg",
+		"04200003.023.0.jpg",
+	],
+	"more-story" => ""
+];
