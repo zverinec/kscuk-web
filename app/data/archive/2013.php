11bdaa6ed6692f6c955002eae87e70d5149d35c2
Jan Horacek <me@apophis.cz>
2018-08-11 22:04:20 +0200

    Archive: add year 2013.

---
+++ app/data/archive/2013.php
@@ -0,0 +1,39 @@
+<?php
+$year = [
+	"year" => 2013,
+	"when" => "7.–14. září 2013",
+	"where" => [
+		"place" => "Mohelenský Mlýn",
+		"map" => "https://mapy.cz/s/28VF9"
+	],
+	"who" => [
+		["person" => "Jiří Maara Marek", "team" => "ksi"],
+		["person" => "Jana Fabriková", "team" => "ksi"],
+		["person" => "Honza Datel Budka", "team" => "ibis"],
+		["person" => "Martin Tíman Hanžl", "team" => "ksi"],
+		["person" => "Jiří Novotný", "team" => "ksi"],
+		["person" => "Henrich Heňo Lauko", "team" => "ksi"],
+		["person" => "Evka Krásenská", "team" => "ibis"],
+		["person" => "Honza Drábek", "team" => "ksi"],
+		["person" => "Tom Effenberger", "team" => "ksi"],
+	],
+	"story" => "Měření průtoku náhonem, KSI a KEKS, přednášky v kuchyni, "
+		."Datel, legendární civilizace a noční hra, nestrukturované volno"
+		." s létajícími hořícími vlaštovkami, vlajkovka, táboráky, workshopy, "
+		."projekty, přednášky ve spacácích, kreativní programy, šifrovačka.",
+	"gallery" => [
+		"IMG_000.jpg",
+		"IMG_001.jpg",
+		"IMG_002.jpg",
+		"IMG_003.jpg",
+		"IMG_004.jpg",
+		"IMG_005.jpg",
+		"IMG_006.jpg",
+		"IMG_007.jpg",
+		"IMG_008.jpg",
+		"IMG_009.jpg",
+		"IMG_010.jpg",
+		"IMG_011.jpg",
+	],
+	"more-story" => ""
+];
