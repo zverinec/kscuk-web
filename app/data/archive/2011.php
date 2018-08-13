fb6d87257e12ea93afcbebb2473ce588e4bee18f
Jan Horacek <me@apophis.cz>
2018-08-13 21:21:21 +0200

    Archive: add year 2011.

---
+++ app/data/archive/2011.php
@@ -0,0 +1,40 @@
+<?php
+$year = [
+	"year" => 2011,
+	"when" => "3.–9. září 2011",
+	"where" => [
+		"place" => "Strážná u Lelekovic",
+		"map" => "https://mapy.cz/s/2UTpG"
+	],
+	"who" => [
+		["person" => "Pavel Troubil", "team" => "ksi"],
+		["person" => "Jana Fabriková", "team" => "ksi"],
+		["person" => "Jan Papi Papoušek", "team" => "ksi"],
+		["person" => "Petr Buršík", "team" => ""],
+		["person" => "Zuzka Petruchová", "team" => "?"],
+		["person" => "Evka Krásenská", "team" => "ibis"],
+		["person" => "Terka Náhliková", "team" => "ibis"],
+		["person" => "Džestr", "team" => "?"],
+		["person" => "Terka Doležalová", "team" => "ksi"],
+		["person" => "Roman Štěpánek", "team" => "?"],
+		["person" => "Jana Tůmová", "team" => "ksi"],
+		["person" => "Mária Běhalová", "team" => "ibis"],
+		["person" => "Terka Procházková", "team" => "ibis"],
+	],
+	"story" => "TODO",
+	"gallery" => [
+		"IMG_000.jpg",
+		"IMG_001.jpg",
+		"IMG_002.jpg",
+		"IMG_003.jpg",
+		"IMG_004.jpg",
+		"IMG_005.jpg",
+		"IMG_006.jpg",
+		"IMG_007.jpg",
+		"IMG_008.jpg",
+		"IMG_009.jpg",
+		"IMG_010.jpg",
+		"IMG_011.jpg",
+	],
+	"more-story" => ""
+];
