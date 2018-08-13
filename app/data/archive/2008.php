fd6e5b8d920243058e7277e2673f8d8ed5ef4635
Jan Horacek <me@apophis.cz>
2018-08-13 22:35:06 +0200

    Archive: add year 2008.

---
+++ app/data/archive/2008.php
@@ -0,0 +1,39 @@
+<?php
+$year = [
+	"year" => 2008,
+	"when" => "září 2008",
+	"where" => [
+		"place" => "Kaprálův mlýn",
+		"map" => "https://mapy.cz/s/2WOvV"
+	],
+	"who" => [
+		["person" => "Radek Pelánek", "team" => "ksi"],
+		["person" => "Pavel Šimeček", "team" => "?"],
+		["person" => "Ondráš Přibyla", "team" => "kafe"],
+		["person" => "Vojta Procházka", "team" => "kafe"],
+		["person" => "Terka Náhliková", "team" => "ibis"],
+		["person" => "Pavel Troubil", "team" => "ksi"],
+		["person" => "Jana Fabriková", "team" => "ksi"],
+		["person" => "Iva Svačinová", "team" => "ibis"],
+		["person" => "Petra Svačinová", "team" => "ibis"],
+		["person" => "Jana Tůmová", "team" => "ksi"],
+		["person" => "Bára Pelánková", "team" => "ksi"],
+		["person" => "Honza Budka", "team" => "ksi"],
+	],
+	"story" => "TODO",
+	"gallery" => [
+		"IMG_000.jpg",
+		"IMG_001.jpg",
+		"IMG_002.jpg",
+		"IMG_003.jpg",
+		"IMG_004.jpg",
+		"IMG_005.jpg",
+		"IMG_006.jpg",
+		"IMG_007.jpg",
+		"IMG_008.jpg",
+		"IMG_009.jpg",
+		"IMG_010.jpg",
+		"IMG_011.jpg",
+	],
+	"more-story" => ""
+];
