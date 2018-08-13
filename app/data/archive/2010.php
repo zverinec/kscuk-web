4e8af5499f39a3ca8d370ed921cb67bb9c94c9a1
Jan Horacek <me@apophis.cz>
2018-08-13 21:59:31 +0200

    Archive: add year 2010.

---
+++ app/data/archive/2010.php
@@ -0,0 +1,40 @@
+<?php
+$year = [
+	"year" => 2010,
+	"when" => "6.–12. září 2010",
+	"where" => [
+		"place" => "Horní mlýn, Ochoz u Brna",
+		"map" => "https://mapy.cz/s/2WO7B"
+	],
+	"who" => [
+		["person" => "Evka Krásenská", "team" => "ibis"],
+		["person" => "Iva Svačilová", "team" => "ibis"],
+		["person" => "Jan Papi Papoušek", "team" => "ksi"],
+		["person" => "Jan Rygl", "team" => "ksi"],
+		["person" => "Jana Fabriková", "team" => "ksi"],
+		["person" => "Ondra Bouda", "team" => "ksi"],
+		["person" => "Pavel Troubil", "team" => "ksi"],
+		["person" => "Petr Jarušek", "team" => "?"],
+		["person" => "Petra Svačilová", "team" => "ibis"],
+		["person" => "Terka Doležalová", "team" => "ksi"],
+		["person" => "Terka Náhliková", "team" => "ibis"],
+		["person" => "Sven Dražan", "team" => "ksi"],
+		["person" => "Hanka Jirků", "team" => "?"],
+	],
+	"story" => "TODO",
+	"gallery" => [
+		"IMG_000.jpg",
+		"IMG_001.jpg",
+		"IMG_002.jpg",
+		"IMG_003.jpg",
+		"IMG_004.jpg",
+		"IMG_005.jpg",
+		"IMG_006.jpg",
+		"IMG_007.jpg",
+		"IMG_008.jpg",
+		"IMG_009.jpg",
+		"IMG_010.jpg",
+		"IMG_011.jpg",
+	],
+	"more-story" => ""
+];
