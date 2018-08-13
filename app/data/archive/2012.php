c02b8ea7773e5bc3bb11c4762c8caaf0d7b22d06
Jan Horacek <me@apophis.cz>
2018-08-13 20:58:48 +0200

    Archive: add year 2012.

---
+++ app/data/archive/2012.php
@@ -0,0 +1,35 @@
+<?php
+$year = [
+	"year" => 2012,
+	"when" => "8.–14. září 2012",
+	"where" => [
+		"place" => "Dobrá voda u Třebíče",
+		"map" => "https://mapy.cz/s/2WNjr"
+	],
+	"who" => [
+		["person" => "Ondra Bouda", "team" => "ksi"],
+		["person" => "Jana Tůmová", "team" => "ksi"],
+		["person" => "Jana Fabriková", "team" => "ksi"],
+		["person" => "Zuzka Petruchová", "team" => "?"],
+		["person" => "Honza Drábek", "team" => "ksi"],
+		["person" => "Jiří Maara Marek", "team" => "ksi"],
+		["person" => "Hanka Lara Kozubíková", "team" => "?"],
+		["person" => "Hanka Jirků", "team" => "?"],
+	],
+	"story" => "TODO",
+	"gallery" => [
+		"IMG_000.jpg",
+		"IMG_001.jpg",
+		"IMG_002.jpg",
+		"IMG_003.jpg",
+		"IMG_004.jpg",
+		"IMG_005.jpg",
+		"IMG_006.jpg",
+		"IMG_007.jpg",
+		"IMG_008.jpg",
+		"IMG_009.jpg",
+		"IMG_010.jpg",
+		"IMG_011.jpg",
+	],
+	"more-story" => ""
+];
