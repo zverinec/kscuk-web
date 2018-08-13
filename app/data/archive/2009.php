25a3d7c4c339330cd3411f3b70af6f0cf6f6937e
Jan Horacek <me@apophis.cz>
2018-08-13 22:27:59 +0200

    Archive: add year 2009.

---
+++ app/data/archive/2009.php
@@ -0,0 +1,38 @@
+<?php
+$year = [
+	"year" => 2009,
+	"when" => "7.–13. září 2009",
+	"where" => [
+		"place" => "Kaprálův mlýn",
+		"map" => "https://mapy.cz/s/2WOvV"
+	],
+	"who" => [
+		["person" => "Terka Náhliková", "team" => "ibis"],
+		["person" => "Jana Fabriková", "team" => "ksi"],
+		["person" => "Pavel Troubil", "team" => "ksi"],
+		["person" => "Ondra Bouda", "team" => "ksi"],
+		["person" => "Iva Svačinová", "team" => "ibis"],
+		["person" => "Petra Svačinová", "team" => "ibis"],
+		["person" => "Jan Papi Papoušek", "team" => "ksi"],
+		["person" => "Terka Doležalová", "team" => "ksi"],
+		["person" => "Hanka Jirků", "team" => "?"],
+		["person" => "Evka Krásenská", "team" => "ibis"],
+		["person" => "Radek Pelánek", "team" => "ksi"],
+	],
+	"story" => "TODO",
+	"gallery" => [
+		"IMG_000.jpg",
+		"IMG_001.jpg",
+		"IMG_002.jpg",
+		"IMG_003.jpg",
+		"IMG_004.jpg",
+		"IMG_005.jpg",
+		"IMG_006.jpg",
+		"IMG_007.jpg",
+		"IMG_008.jpg",
+		"IMG_009.jpg",
+		"IMG_010.jpg",
+		"IMG_011.jpg",
+	],
+	"more-story" => ""
+];
