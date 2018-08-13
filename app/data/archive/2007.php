02e23bd629c6424ca40fdce8609610dc22507d48
Jan Horacek <me@apophis.cz>
2018-08-13 22:41:24 +0200

    Archive: add year 2007.

---
+++ app/data/archive/2007.php
@@ -0,0 +1,34 @@
+<?php
+$year = [
+	"year" => 2007,
+	"when" => "2007",
+	"where" => [
+		"place" => "Kaprálův mlýn",
+		"map" => "https://mapy.cz/s/2WOvV"
+	],
+	"who" => [
+		["person" => "Radek Pelánek", "team" => "ksi"],
+		["person" => "Ondráš Přibyla", "team" => "kafe"],
+		["person" => "Pavel Šimeček", "team" => "?"],
+		["person" => "Pavel Troubil", "team" => "ksi"],
+		["person" => "Bára Pelánková", "team" => "ksi"],
+		["person" => "Terka Náhliková", "team" => "ibis"],
+		["person" => "Honza HP Pavelka", "team" => "kafe"],
+		["person" => "Vojta Procházka", "team" => "kafe"],
+		["person" => "Jana Tůmová", "team" => "ksi"],
+		["person" => "Jana Fabriková", "team" => "ksi"],
+	],
+	"story" => "TODO",
+	"gallery" => [
+		"04200017.001.0.jpg",
+		"04200017.002.0.jpg",
+		"04200017.003.0.jpg",
+		"04200017.004.0.jpg",
+		"04200017.005.0.jpg",
+		"04200017.006.0.jpg",
+		"04200017.007.0.jpg",
+		"04200017.008.0.jpg",
+		"04200017.009.0.jpg",
+	],
+	"more-story" => ""
+];
