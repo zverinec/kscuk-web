96312f77b95662f7ad668e1daf571eaf9ca49098
Vendula Němcová <v.nem.co@gmail.com>
2018-08-19 17:03:21 +0200

    View health declaration without code if logged in as org. #51

--- app/presenters/FormsPresenter.php
+++ app/presenters/FormsPresenter.php
@@ -24,12 +24,35 @@ class FormsPresenter extends BasePresenter
 
 	public function renderFilledForm($email, $code)
 	{
-		$hd = $this->healthDeclaration;
-		$values = $hd->findByEmail($email)->fetchAll();
-		$values = $values[0];
+		$values = $this->getHealthDeclaration($email);
 		if ($code != $values["code"]) {
 			throw new BadRequestException('Wrong code', 404);
 		}
+		$this->sendAsPDF($values);
+	}
+
+	public function renderFilledFormOrg($email)
+	{
+		if (!$this->user->isLoggedIn()) {
+			$this->flashMessage("Neoprávněný přístup do organizátorské sekce!", "error");
+			$this->redirect("Org:auth");
+		}
+		$values = $this->getHealthDeclaration($email);
+		$this->sendAsPDF($values);
+	}
+
+	public function createComponentHealthDeclaration()
+	{
+		return $this->healthDeclarationFactory->create();
+	}
+
+	private function getHealthDeclaration($email) {
+		$hd = $this->healthDeclaration;
+		$values = $hd->findByEmail($email)->fetchAll();
+		return $values[0];
+	}
+
+	private function sendAsPDF($values) {
 		$template = $this->createTemplate();
 		$template->setFile(__DIR__ . "/../templates/filledForm.latte");
 		$template->v = $values;
@@ -39,10 +62,4 @@ class FormsPresenter extends BasePresenter
 		$this->sendResponse($pdf);
 		$this->terminate();
 	}
-
-	public function createComponentHealthDeclaration()
-	{
-		return $this->healthDeclarationFactory->create();
-	}
-
 }
