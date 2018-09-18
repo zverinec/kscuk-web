e6a5352e0b8d448da4fad3b206834cb2affc3fa0
Jan Horacek <me@apophis.cz>
2018-09-18 13:49:45 +0200

    Archive: add year 2018.

---
+++ app/data/archive/2018.php
@@ -0,0 +1,27 @@
+<?php
+$year = [
+	"year" => 2018,
+	"when" => "9.–16. září 2018",
+	"where" => [
+		"place" => "Mohelenský mlýn",
+		"map" => "https://mapy.cz/s/28VF9"
+	],
+	"who" => [
+		["person" => "Honza Horáček", "team" => "ksi"],
+		["person" => "Anička Řechtáčková", "team" => "ksi"],
+		["person" => "Dominika Krejčí", "team" => "ksi"],
+		["person" => "Honza Mrázek", "team" => "ksi"],
+		["person" => "Viki Vozárová", "team" => "ksi"],
+		["person" => "Pavla Havlíčková", "team" => "ibis"],
+		["person" => "Standa Juračka", "team" => "ibis"],
+		["person" => "Zuzka Nováková", "team" => "ibis"],
+	],
+	"story" => "Mlýn, divadlo, kíšťata, požární poplach, Darwin v kuchyni, "
+		."Kamasutra race, Rescue Mission, čajovna, tancování, šerm, přeplavávání"
+		." Mohelna, „Uhel syslům mojí pecí včelám neutavím“ a další šifry, oběd"
+		." na hradě, Just dance, slovní fotbal ve třídě PSPACE, IBIS v novém "
+		."se Zuzkou ve vedení. Termit. A samozřejmě KSI.",
+	"gallery" => [
+	],
+	"more-story" => ""
+];
