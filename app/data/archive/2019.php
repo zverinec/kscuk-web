bfa1e693b5f249fc3ee191864448356071b9cfd7
Jan Horacek <me@apophis.cz>
2019-09-15 22:25:49 +0200

    Archive: add 2019 event basic information.

---
+++ app/data/archive/2019.php
@@ -0,0 +1,22 @@
+<?php
+$year = [
+	"year" => 2019,
+	"when" => "7.–14. září 2019",
+	"where" => [
+		"place" => "Dobrá voda u Třebíče",
+		"map" => "https://mapy.cz/s/mujezoleju"
+	],
+	"who" => [
+		["person" => "Anička Řechtáčková", "team" => "ksi"],
+		["person" => "Honza Horáček", "team" => "ksi"],
+		["person" => "Dominika Krejčí", "team" => "ksi"],
+		["person" => "Veru Burgerová", "team" => "ksi"],
+		["person" => "Tom Effenberger", "team" => "ksi"],
+		["person" => "Zuzka Nováková", "team" => "ibis"],
+		["person" => "Tomáš Janovič", "team" => "ibis"],
+	],
+	"story" => "",
+	"gallery" => [
+	],
+	"more-story" => ""
+];
