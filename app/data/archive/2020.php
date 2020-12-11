790b031d38b5c791da0a344e0892f46eb9cd27d7
Jan Horacek <me@apophis.cz>
2020-12-11 15:59:24 +0100

    Add 2020 to archive.

---
+++ app/data/archive/2020.php
@@ -0,0 +1,50 @@
+<?php
+$year = [
+	"year" => 2020,
+	"when" => "6.–13. září 2020",
+	"where" => [
+		"place" => "Mohelenský mlýn",
+		"map" => "https://mapy.cz/s/28VF9"
+	],
+	"who" => [
+		["person" => "Anička Řechtáčková", "team" => "ksi"],
+		["person" => "Dominika Krejčí", "team" => "ksi"],
+		["person" => "Tom Effenberger", "team" => "ksi"],
+		["person" => "Dano Timko", "team" => "ksi"],
+		["person" => "Edo Ruisl", "team" => "ksi"],
+		["person" => "Adam Hlaváček", "team" => "ksi"],
+		["person" => "Monika Kuncová", "team" => "ibis"],
+		["person" => "Vítek Procházka", "team" => "ibis"],
+	],
+	"story" => "Náročná cesta na Mlýn v době koronavirové, agentský výcvik "
+		."(poprvé v historii K-SCUKu s živým pejskem!), seznamování účastníků "
+		."na sto a jeden způsob, simulace složitých systémů na případu distribuce"
+		." nápojů. Imitace filmových scén = trénink v nenápadnosti, týmové "
+		."vaření večeře. Darwin a Zuzka v kuchyni. Automatizace hraní "
+		."dinosaura, měření tlaku pro zisk optimálního plánu rozcvičky, "
+		."jazyk pro popis tanečních kroků a další. Zkoumání vlastností hadcové"
+		." stepi. Samozřejmě šifrovačka. Skládání zkoušky přípravou létajících"
+		." vaflí. Bojové umění, pletení, masírování, výroba praku, pájení, "
+		."pečení. Velice špatná noční hra. Jaký byl svět před dvaceti lety? "
+		."Jaký bude za dalších dvacet? A na závěr zisk imunity proti COWID "
+		."(corn worm infectious disease) a dobytí Mlýnu zpět od mimozemských "
+		."extrudovaných červů."
+		."<p>Ročník byl realizován díky podpoře Ministerstva školství, mládeže a "
+		."tělovýchovy, dotace na podporu nadaných žáků základních a středních "
+		."škol v roce 2020 (projekt č. 0039/7/NAD/2020).</p>",
+	"gallery" => [
+		"k-scuk-2020-01.jpg",
+		"k-scuk-2020-05.jpg",
+		"k-scuk-2020-118.jpg",
+		"k-scuk-2020-149.jpg",
+		"k-scuk-2020-150.jpg",
+		"k-scuk-2020-157.jpg",
+		"k-scuk-2020-202.jpg",
+		"k-scuk-2020-232.jpg",
+		"k-scuk-2020-258.jpg",
+		"k-scuk-2020-303.jpg",
+		"k-scuk-2020-80.jpg",
+		"k-scuk-2020-94.jpg",
+	],
+	"more-story" => ""
+];
