1f1ae691fb354c0e6e2b6ef69f1e4e2ef69caa1f
Jan Horacek <me@apophis.cz>
2021-11-28 19:31:34 +0100

    Add 2021 event to archive.

---
+++ app/data/archive/2021.php
@@ -0,0 +1,45 @@
+<?php
+$year = [
+	"year" => 2021,
+	"when" => "5.–12. září 2021",
+	"where" => [
+		"place" => "Fara Bedřichov",
+		"map" => "https://mapy.cz/s/2Vmt5"
+	],
+	"who" => [
+		["person" => "Anička Řechtáčková", "team" => "ksi"],
+		["person" => "Verča Burgerová", "team" => "ksi"],
+		["person" => "Monika Kuncová", "team" => "ibis"],
+		["person" => "Bára Dohnalová", "team" => "ksi"],
+		["person" => "Honza Horáček", "team" => "ksi"],
+		["person" => "Jarek Rohel", "team" => "ibis"],
+		["person" => "Franta Váňa", "team" => "ibis"],
+		["person" => "Kuba Šťastný", "team" => "ksi"],
+	],
+	"story" => "Po sedmi letech opět v Bedřichově (včetně nalezeného "
+		."zapomenutého zápisníku!). Navzdory covidu plno za KSI i IBIS. "
+		."Výsadek maskovaný za šifrovačku a spousta vysílaček, pájení 3D LED "
+		."krychle nebo vaření cibulačky; opravdová šifrovačka s určováním PH "
+		."červeným zelím; trénink psychické první pomoci a zkouška z biologie "
+		."a informatiky (s háčkem). Hledání záškodníka, kde žádný nebyl. Spaní "
+		."s paní. A soutěž cestovních kanceláří, která se zvrhla v evakuaci "
+		."před hurikánem. Bylinkové večerníčky a memes. A LEGO!"
+		."<p>Ročník byl realizován díky podpoře Ministerstva školství, mládeže a "
+		."tělovýchovy, dotace na podporu nadaných žáků základních a středních "
+		."škol v roce 2021.</p>",
+	"gallery" => [
+		"Z50_0991.jpeg",
+		"Z50_1173.jpeg",
+		"Z50_1462.jpeg",
+		"Z50_1657.jpeg",
+		"Z50_1737.jpeg",
+		"Z50_1959.jpeg",
+		"Z50_2427.jpeg",
+		"Z50_2980.jpeg",
+		"Z50_3006.jpeg",
+		"Z50_3137.jpeg",
+		"Z50_3483.jpeg",
+		"memes.png",
+	],
+	"more-story" => ""
+];
