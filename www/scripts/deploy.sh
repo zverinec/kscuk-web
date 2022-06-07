105812824f9efaf062fca4ff67d1c6e423b45185
Jan Horacek <me@apophis.cz>
2022-06-07 22:46:16 +0200

    Move deploy.sh to separate directory.

--- www/scripts/deploy.sh
+++ www/scripts/deploy.sh
@@ -8,7 +8,7 @@ if [ $? -eq 0 ]; then
 fi
 
 if [ $? -eq 0 ]; then
-	OUTPUT+=$(cd .. && rm -rf ./temp/cache 2>&1)$'\n\n'
+	OUTPUT+=$(cd ../.. && rm -rf ./temp/cache 2>&1)$'\n\n'
 fi
 
 echo "$OUTPUT" | mail "kscuk@fi.muni.cz" -s "[kscuk-web] Deploy status"
