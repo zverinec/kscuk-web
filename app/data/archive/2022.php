dfc87c1e06b7534aec7ac7265c1ae2cdcef4f0dc
Anna Rechtackova <xrechtac@fi.muni.cz>
2022-10-11 12:56:08 +0200

    Add 2022 event to archive

---
+++ app/data/archive/2022.php
@@ -0,0 +1,46 @@
+<?php
+$year = [
+	"year" => 2022,
+	"when" => "3.–10. září 2022",
+	"where" => [
+		"place" => "TZ Junior",
+		"map" => "https://mapy.cz/s/pubekovefe"
+	],
+	"who" => [
+		["person" => "Anička Řechtáčková", "team" => "ksi"],
+		["person" => "Verča Burgerová", "team" => "ksi"],
+		["person" => "Monika Kuncová", "team" => "ibis"],
+		["person" => "Bára Dohnalová", "team" => "ksi"],
+		["person" => "Hanka Slámová", "team" => "ibis"],
+		["person" => "Jarek Rohel", "team" => "ibis"],
+		["person" => "Franta Váňa", "team" => "ibis"],
+		["person" => "Kuba Šťastný", "team" => "ksi"],
+		["person" => "Karel Horáček", "team" => "ksi"],
+	],
+	"story" => "Poprvé na dobrodružné základně TZ Junior. Začátek procházkou "
+        ."po Vranově a nalezením mrtvoly. Seznamování, i poslepu. Vyšetřování "
+        ."záhadné italské mafie řídící továrnu na toaleťák. Vypékání železa "
+        ."z krve, zkoumání limitů výpočetních systémů, kultivace bakterií, "
+        ."LED pásek na dálkové ovládání. Zkouška znalostí s háčkem (pro veliký "
+        ."úspěch!). Zneškodňování bomby a pobyt v krytu. Celý den navíc! "
+        ."(Za všechno, co se pokazil, samozřejmě mohl org Karel). "
+        ."Spousta italského přízvuku. A nakonec velká oslavná pouť."
+		."<p>Ročník byl realizován díky podpoře Ministerstva školství, mládeže a "
+		."tělovýchovy, dotace na podporu nadaných žáků základních a středních "
+		."škol v roce 2021.</p>",
+	"gallery" => [
+		"Z50_0991.jpeg",
+		"Z50_1173.jpeg",
+		"Z50_1462.jpeg",
+		"Z50_1657.jpeg",
+		"Z50_1737.jpeg",
+		"Z50_1959.jpeg",
+		"Z50_2427.jpeg",
+		"Z50_2980.jpeg",
+		"Z50_3006.jpeg",
+		"Z50_3137.jpeg",
+		"Z50_3483.jpeg",
+		"memes.png",
+	],
+	"more-story" => ""
+];
