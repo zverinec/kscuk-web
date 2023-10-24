bfae9a9768cc6ce140f3247028be6814309c7c78
Anna Rechtackova <xrechtac@fi.muni.cz>
2023-10-24 15:54:22 +0200

    Add K-SCUK 2023 to archive

---
+++ app/data/archive/2023.php
@@ -0,0 +1,49 @@
+<?php
+$year = [
+	"year" => 2023,
+	"when" => "9.–14. září 2022",
+	"where" => [
+		"place" => "Dobrá voda u Třebíče",
+		"map" => "https://mapy.cz/s/mujezoleju"
+	],
+	"who" => [
+		["person" => "Jarek Rohel", "team" => "ibis"],
+		["person" => "Verča Burgerová", "team" => "ksi"],
+		["person" => "Magdalena Dohnalová", "team" => "ibis"],
+        ["person" => "Vojta Klapetek", "team" => "ibis"],
+		["person" => "Monika Kuncová", "team" => "ibis"],
+		["person" => "Eliška Pirnosová", "team" => "ibis"],
+		["person" => "Hanka Slámová", "team" => "ibis"],
+		["person" => "Jakub Šťastný", "team" => "ksi"],
+		["person" => "František Váňa", "team" => "ibis"],
+		["person" => "Karel Horáček", "team" => "ksi"],
+	],
+	"story" => "Po čtyřech letech opět na Dobré Vodě. KSI i IBIS. "
+        ."Hledání organizátorů v Třebíči. Osídlení Dobré Vody mloky, "
+        ."které překazilo plány minerálníkům. Táborák a únos, který se "
+        ."změnil v šifrovačku. Zkoumání kvality (Dobré) vody, hledání "
+        ."parazitů v objektu i před ním, skládání Rubikovy kostky a výroba "
+        ."laser game. Epická RPG hra s nácvikem formací, hledáním lootboxů a "
+        ."nočními odpaly nejen pyrotechniky. Zachraňování rakety, barevná "
+        ."tlačítka a chlazení motoru. Komunikační workshop. Honza a Elí v "
+        ."kuchyni. A samozřejmě mloci, velemloci, víc mloků, Mlokoř a další "
+        "mločí písně."
+		."<p>Ročník byl realizován díky podpoře Ministerstva školství, mládeže a "
+		."tělovýchovy, dotace na podporu nadaných žáků základních a středních "
+		."škol v roce 2021.</p>",
+	"gallery" => [
+        "IMG_08007.JPG",
+        "IMG_09224.JPG",
+        "IMG_10111.JPG",
+        "IMG_10862.JPG",
+        "IMG_08583.JPG",
+        "IMG_09337.JPG",
+        "IMG_10354.JPG",
+        "IMG_11075.JPG",
+        "IMG_09143.JPG",
+        "IMG_09754.JPG",
+        "IMG_10423.JPG",
+        "IMG_11097.JPG",
+	],
+	"more-story" => ""
+];
