b27e71ed29478e94487dae0ac46044efd131429e
Michael Hrubý <galactus2121@gmail.com>
2025-05-13 15:58:18 +0200

    Archiving year 2024

---
+++ app/data/archive/2024.php
@@ -0,0 +1,49 @@
+<?php
+$year = [
+	"year" => 2024,
+	"when" => "7.–14. září 2024",
+	"where" => [
+		"place" => "TZ Junior",
+		"map" => "https://mapy.cz/s/pubekovefe"
+	],
+	"who" => [
+		["person" => "Jarek Rohel", "team" => "ibis"],
+		["person" => "Marek Hába", "team" => "ksi"],
+		["person" => "Vítek Procházka", "team" => "ibis"],
+    ["person" => "Jindra Halabala", "team" => "ksi"],
+		["person" => "Dorka Vichová", "team" => "ibis"],
+		["person" => "Kuba Šťastný", "team" => "ksi"],
+		["person" => "Vojta Klapetek", "team" => "ibis"],
+		["person" => "Ivča Kasprzaková", "team" => "ksi"],
+	],
+  "story" => "Podruhé na břehu Vranovské přehrady. Neuváženě podaná "
+        ."grantová žádost, kvůli které se stáváme se zaměstnanci "
+        ."inovativní dopravní firmy EAAD. Výzkum balónků a balónů všech "
+        ."tvarů a způsobilostí. Letecký souboj vzducholodí a výcvik v "
+        ."pečení nejlepších létajících vaflí. Hledání nejlepšího "
+        ."programovacího jazyku, testování kvality vody v přilehlé přehradě "
+        ."i techniky přežití v tatranské divočině. Volební machinace "
+        ."s balónky. Odhalení skandálního příběhu bratrů Montgolfiérů "
+        ."a nakonec i nález jejich legendárního prototypu horkovzdušného "
+        ."balónu. Bagr. A na závěr opěvovaná tlaková níž přinesla povodně, "
+        ."které zasáhly celou republiku. Navzdory tomu jsme ale všichni "
+        ."zvládli zdárně odplout, totiž odletět."
+		."<p>Ročník byl realizován díky podpoře Ministerstva školství, mládeže a "
+		."tělovýchovy, dotace na podporu nadaných žáků základních a středních "
+		."škol v roce 2024.</p>",
+	"gallery" => [
+        "IMG_3746.JPG",
+        "IMG_3769.JPG",
+        "IMG_4366.JPG",
+        "IMG_4766.JPG",
+        "IMG_5832.JPG",
+        "IMG_6356.JPG",
+        "IMG_6872.JPG",
+        "IMG_7291.JPG",
+        "IMG_7962.JPG",
+        "IMG_8947.JPG",
+        "IMG_9020.JPG",
+        "IMG_9041.JPG",
+	],
+	"more-story" => ""
+];
