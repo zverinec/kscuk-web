6f0380fa496f1d0e48f2fbfd02cc5e4de16226f3
Michael Hrubý <galactus2121@gmail.com>
2025-10-08 01:04:35 +0200

    Change: Archiving year 2025

---
+++ app/data/archive/2025.php
@@ -0,0 +1,48 @@
+<?php
+$year = [
+	"year" => 2025,
+	"when" => "6.–13. září 2025",
+	"where" => [
+		"place" => "Dobrá voda u Třebíče",
+		"map" => "https://mapy.cz/s/mujezoleju"
+	],
+	"who" => [
+		["person" => "Jarek Rohel", "team" => "ibis"],
+		["person" => "Michael Hrubý", "team" => "ksi"],
+		["person" => "Eliška Pirnosová", "team" => "ibis"],
+    ["person" => "Jindra Halabala", "team" => "ksi"],
+		["person" => "Radka Sedláčková", "team" => "ibis"],
+		["person" => "Kuba Šťastný", "team" => "ksi"],
+		["person" => "Martin Richter", "team" => "ibis"],
+		["person" => "Martin Doušek", "team" => "ksi"],
+		["person" => "Anička Řechtáčková", "team" => "ksi"],
+	],
+  "story" => "Útěk přerostlých banánů z laboratoře na Dobré Vodě. Velké banány "
+        ."spatřeny v okolí a způsobují paniku. Boj o královský trůn. "
+        ."Vezme se Romeo a Julie? Záchrana lidských srdcí i nácvik "
+        ."první pomoci po dopravní nehodě. Bomba-stický projekt, tvorba "
+        ."programu na vizualizaci Rubikovy kostky, izolace DNA nejen "
+        ."z banánu a složení Banánové republiky. Noční můra, ze které vás "
+        ."neprobudí ani Tramtadadááá. Vyvolávání oživlých banánů. "
+        ."Zákeřné banány v okolí Dobré Vody. Lekce vaření a stavby věže "
+        ."(samozřejmě s banány). První banán letí do vesmíru! Nebo ne? "
+        ."Úspěšný start firmy Naše banány České banány a nakonec společné "
+        ."zneškodnění nebezpečné bomby za pomoci Velkého špagetového monstra."
+		."<p>Ročník byl realizován s podporou Fakulty informatiky a Přírodovědecké "
+    ."fakulty Masarykovy univerzity.</p>",
+	"gallery" => [
+        "046-IMG_3506.JPG",
+        "106-IMG_5913.JPG",
+        "112-IMG_6046.JPG",
+        "140-IMG_6685.JPG",
+        "156-IMG_6982.JPG",
+        "182-IMG_7560.JPG",
+        "203-IMG_8154.JPG",
+        "237-IMG_8938.JPG",
+        "255-IMG_9487.JPG",
+        "269-IMG_9785.JPG",
+        "318-IMG_1250.JPG",
+        "414-IMG_4584.JPG",
+	],
+	"more-story" => ""
+];
